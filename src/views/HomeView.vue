b1f70a72a408cc536cc53c3eced505452a11200d
Mostafa <mostafakooti2018@gmail.com>
2023-02-26 22:06:26 +0330

    ./

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,27 +1,11 @@
 <template>
   <div class="Home">
-    <h1>Home</h1>
-    <br>
-    <div>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident corporis est, unde voluptatem iusto quis
-      natus? Consequatur, corrupti distinctio eaque quia possimus ratione nam sapiente laudantium deleniti? Saepe, dolorem
-      itaque!
-    </div>
-    <hr>
-    <div>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident corporis est, unde voluptatem iusto quis
-      natus? Consequatur, corrupti distinctio eaque quia possimus ratione nam sapiente laudantium deleniti? Saepe, dolorem
-      itaque!
-    </div>
-    <hr>
-    <div>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident corporis est, unde voluptatem iusto quis
-      natus? Consequatur, corrupti distinctio eaque quia possimus ratione nam sapiente laudantium deleniti? Saepe, dolorem
-      itaque!
-    </div>
-    <hr>
-    <div>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident corporis est, unde voluptatem iusto quis
-      natus? Consequatur, corrupti distinctio eaque quia possimus ratione nam sapiente laudantium deleniti? Saepe, dolorem
-      itaque!
-    </div>
-    <hr>
+    <h1 class="text-center mb-5">Home</h1>
+      <article v-for="article in articles">
+        <h3><a :href="'/article/' + article.slug">{{ article.title }}</a></h3>
+        <div>{{ article.description }} <a :href="'/article/' + article.slug">+ more</a></div>
+        <hr>
+      </article>
   </div>
 </template>
 
@@ -29,5 +13,41 @@
 
 export default {
   name: 'HomeView',
+  data() {
+    return {
+      articles: [
+      {
+          title: "Title 1",
+          slug: "title-1",
+          description: "Lorem ipsum dolor sit amet dwidnoa gmcaosnd erlrka. djwiadioa ionmanfnfam dnwof mdwma pm pimjp?",
+          body: "Lorem ipsum dolor sit amet dwidnoa gmcaosnd erlrka. djwiadioa ionmanfnfam dnwof mdwma pm pimjp?",
+        },
+        {
+          title: "Title 2",
+          slug: "title-2",
+          description: "Lorem ipsum dolor sit amet dwidnoa gmcaosnd erlrka. djwiadioa ionmanfnfam dnwof mdwma pm pimjp?",
+          body: "Lorem ipsum dolor sit amet dwidnoa gmcaosnd erlrka. djwiadioa ionmanfnfam dnwof mdwma pm pimjp?",
+        },
+        {
+          title: "Title 3",
+          slug: "title-3",
+          description: "Lorem ipsum dolor sit amet dwidnoa gmcaosnd erlrka. djwiadioa ionmanfnfam dnwof mdwma pm pimjp?",
+          body: "Lorem ipsum dolor sit amet dwidnoa gmcaosnd erlrka. djwiadioa ionmanfnfam dnwof mdwma pm pimjp?",
+        },
+        {
+          title: "Title 4",
+          slug: "title-4",
+          description: "Lorem ipsum dolor sit amet dwidnoa gmcaosnd erlrka. djwiadioa ionmanfnfam dnwof mdwma pm pimjp?",
+          body: "Lorem ipsum dolor sit amet dwidnoa gmcaosnd erlrka. djwiadioa ionmanfnfam dnwof mdwma pm pimjp?",
+        },
+        {
+          title: "Title 5",
+          slug: "title-5",
+          description: "Lorem ipsum dolor sit amet dwidnoa gmcaosnd erlrka. djwiadioa ionmanfnfam dnwof mdwma pm pimjp?",
+          body: "Lorem ipsum dolor sit amet dwidnoa gmcaosnd erlrka. djwiadioa ionmanfnfam dnwof mdwma pm pimjp?",
+        }
+      ]
+    }
+  }
 }
 </script>
